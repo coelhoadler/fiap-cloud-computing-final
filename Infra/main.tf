b0d9c07b997f143c27ac44a73c1e3f84fde63daa
Kelvin Marques <kelvin.marques@gmail.com.br>
2020-12-06 18:22:06 -0300

    TF dlq

--- Infra/main.tf
+++ Infra/main.tf
@@ -2,9 +2,20 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_sqs_queue" "terraform_queue_deadletter" {
+    name = "SQS-principal-${terraform.workspace}-dlq"
+    tags = {
+        Name = "SQS-principal-${terraform.workspace}-dlq"
+    }
+}
+
 resource "aws_sqs_queue" "terraform_queue" {
     name = "SQS-principal-${terraform.workspace}"
     tags = {
         Name = "SQS-principal-${terraform.workspace}"
     }
+    redrive_policy = jsonencode({
+        deadLetterTargetArn: aws_sqs_queue.terraform_queue_deadletter.arn,
+        maxReceiveCount = 4
+    })
 }
